7a00ec3d9cc8d4b12b76f93f6ff863f3c008db34
Erik Ricardo Hass <erikricardohass@gmail.com>
2022-09-04 20:27:40 -0300

    Chekin

---
+++ Network.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BennerTesteLogica
+{
+    public class Network
+    {
+        private Elemento[] _elementos;
+        public Network(long numeroElementos)
+        {
+            if (numeroElementos < 2)
+            {
+                throw new Exception("A classe network deve conter pelo menos 2 elementos.");
+            }
+
+            _elementos = new Elemento[numeroElementos];
+
+            for (int indice = 1; indice <= _elementos.Length; indice++)
+            {
+                _elementos[indice - 1] = new Elemento(indice);
+            }
+        }
+
+        public void Connect(long elementoA, long elementoB)
+        {
+            VerificarElementosMaiorQueZero(elementoA, elementoB);
+            VerificarExistenciaElementos(elementoA, elementoB);
+
+            if (elementoA == elementoB)
+            {
+                return;
+            }
+
+            if (!_elementos.First(E => E.ID == elementoA)._conexoes.Contains(elementoB))
+            {
+                _elementos.First(E => E.ID == elementoA)._conexoes.Add(elementoB);
+                _elementos.First(E => E.ID == elementoB)._conexoes.Add(elementoA);
+            }
+        }
+
+        public bool Query(long elementoA, long elementoB)
+        {
+            VerificarElementosMaiorQueZero(elementoA, elementoB);
+            VerificarExistenciaElementos(elementoA, elementoB);
+
+            if (elementoA == elementoB)
+            {
+                return true;
+            }
+
+            return VerificarConexaoElementos(elementoA, elementoB, new List<long> { elementoA});
+        }
+
+        private bool VerificarConexaoElementos(long elementoA, long elementoB, List<long> elementosVerificados)
+        {
+            if(_elementos.First(E => E.ID == elementoA)._conexoes.Any(C => C == elementoB))
+            {
+                return true;
+            }
+
+            foreach(long elementoConexao in _elementos.First(E => E.ID == elementoA)._conexoes)
+            {
+                if (!elementosVerificados.Contains(elementoConexao))
+                {
+                    elementosVerificados.Add(elementoConexao);
+                    if (VerificarConexaoElementos(elementoConexao, elementoB, elementosVerificados))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void VerificarExistenciaElementos(long elementoA, long elementoB)
+        {
+            if (!_elementos.Any(E => E.ID == elementoA))
+            {
+                throw new Exception($"O elemento {elementoA} não existe no conjunto de elementos.");
+            }
+            if (!_elementos.Any(E => E.ID == elementoB))
+            {
+                throw new Exception($"O elemento {elementoB} não existe no conjunto de elementos.");
+            }
+        }
+
+        private void VerificarElementosMaiorQueZero(long elementoA, long elementoB)
+        {
+            if (elementoA < 1 || elementoB < 1)
+            {
+                throw new Exception($"Nenhum elemento pode ser menor que 1.");
+            }
+        }
+
+        protected struct Elemento
+        {
+            private long _id;
+            public List<long> _conexoes { get; }
+            public long ID { get => _id; }
+
+            
+
+            public Elemento(long id)
+            {
+                this._id = id;
+                _conexoes = new List<long>();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BennerTesteLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Network network = new Network(10);
+            network.Connect(1, 2);
+            network.Connect(1, 1);
+            network.Connect(2, 9);
+            network.Connect(9, 3);
+            network.Connect(3, 7);
+            network.Connect(5, 4);
+
+            Console.WriteLine("1:");
+            Console.WriteLine(network.Query(1, 2));
+            Console.WriteLine(network.Query(1, 9));
+            Console.WriteLine(network.Query(1, 3));
+            Console.WriteLine(network.Query(1, 7));
+
+            Console.WriteLine("2:");
+            Console.WriteLine(network.Query(2, 9));
+            Console.WriteLine(network.Query(2, 3));
+            Console.WriteLine(network.Query(2, 7));
+
+            Console.WriteLine("9:");
+            Console.WriteLine(network.Query(9, 3));
+            Console.WriteLine(network.Query(9, 7));
+
+            Console.WriteLine("3:");
+            Console.WriteLine(network.Query(3, 7));
+
+            Console.WriteLine("7:");
+            Console.WriteLine(network.Query(7, 1));
+            Console.WriteLine(network.Query(7, 2));
+            Console.WriteLine(network.Query(7, 9));
+            Console.WriteLine(network.Query(7, 3));
+            Console.WriteLine(network.Query(7, 10));
+
+            Console.WriteLine("5:");
+            Console.WriteLine(network.Query(5, 1));
+            Console.WriteLine(network.Query(5, 2));
+            Console.WriteLine(network.Query(5, 3));
+            Console.WriteLine(network.Query(5, 4));
+            Console.WriteLine(network.Query(5, 6));
+            Console.WriteLine(network.Query(5, 7));
+            Console.WriteLine(network.Query(5, 8));
+            Console.WriteLine(network.Query(5, 9));
+            Console.WriteLine(network.Query(5, 10));
+
+
+            Console.Read();
+        }
+    }
+}
